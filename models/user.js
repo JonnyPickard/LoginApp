f131e3346c8eedd497cef90ed9402ff1c75af458
Jonny Pickard <jonathandpickard@gmail.com>
2016-09-20 19:19:38 +0100

    Login authentication successful

--- models/user.js
+++ models/user.js
@@ -26,6 +26,23 @@ module.exports.createUser = function(newUser, callback) {
   });
 };
 
+module.exports.getUserByUsername = function(username, callback){
+  var query = {username: username};
+  User.findOne(query, callback);
+};
+
+module.exports.getUserById = function(id, callback){
+  User.findById(id, callback);
+};
+
+module.exports.comparePassword = function(candidatePassword, hashPassword, callback){
+  bcrypt.compare(candidatePassword, hashPassword, function(err, isMatch){
+    if(err) throw err;
+    callback(null, isMatch);
+  });
+};
+
+
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -77,6 +77,16 @@ passport.use(new LocalStrategy(
   }
 ));
 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 router.post('/login',
   passport.authenticate('local', {sucessRedirect: '/', failureRedirect: '/users/login', failureFlash: true}),
   function(req, res){
